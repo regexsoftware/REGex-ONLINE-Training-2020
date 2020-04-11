7581e2fc0fba74cfd3ab9edb65e6a6f07e77a81f
Tushar Goyal <tushargoyal19tj@gmail.com>
2020-04-11 20:14:42 +0530

    Online class assignment

---
+++ online-python-series/ifconditions.py
@@ -0,0 +1,21 @@
+
+
+x = int(input("Please enter a number : "))
+#print("value of x", x)
+#print(type(x))
+
+
+y=10
+z=22
+
+# print I am the superhero if x =12
+if(x==14):
+	print("I am superhero")
+elif(y==10):
+	print("value of y is:", y)
+
+elif(z==22):
+	print("printing the value of z:", z)
+
+else:
+	print("if condition is wrong")
