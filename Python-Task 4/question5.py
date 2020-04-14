1f1500cfde2fb10d8101d6b0ef3844ba36952ab4
Shubhang Vats <shubhangvats14@gmail.com>
2020-04-14 15:45:22 +0530

    Task 4 of Python session #4

---
+++ Python-Task 4/question5.py
@@ -0,0 +1,22 @@
+# Question - 5
+
+'''
+Take a list REGex=[1,2,3,4,5,6,7,8,9,0,77,44,15,33,65,89,12]
+    - print only those numbers greator then 20
+    - then print those numbers those are less then 10 or equal to 10
+    - store these above two list in two different list.
+
+'''
+a=[] #list of those numbers greator then 20
+b=[] #list of those numbers those are less then 10 or equal to 10
+
+REGex=[1,2,3,4,5,6,7,8,9,0,77,44,15,33,65,89,12]
+for i in REGex:
+    if i>20:
+        a.append(i)
+    elif i <= 10:
+        b.append(i)
+        
+print(a) #list of those numbers greator then 20
+
+print(b)  #list of those numbers those are less then 10 or equal to 10
