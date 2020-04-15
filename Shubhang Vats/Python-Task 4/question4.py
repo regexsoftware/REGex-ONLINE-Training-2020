38c6f6d22ae9d034890840648c03e7d2650d585c
Shubhang Vats <shubhangvats14@gmail.com>
2020-04-15 16:55:58 +0530

    Python class #4 Task 4

---
+++ Shubhang Vats/Python-Task 4/question4.py
@@ -0,0 +1,14 @@
+# Question - 4
+
+'''
+GIve a tuple:
+    tuple = ('a','l','g','o','r','i','t','h','m')
+        1. Using the concept of slicing, print the whole tuple
+        2. delete the element at the 3rd Index, print the tuple.
+
+'''
+
+t = ('a','l','g','o','r','i','t','h','m')
+
+for i in range(9):
+    print(t[i])
