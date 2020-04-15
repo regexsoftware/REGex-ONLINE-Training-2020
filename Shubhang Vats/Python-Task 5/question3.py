410bc9f8bde89ae4ee0979a43c9bedc100d021b3
Shubhang Vats <shubhangvats14@gmail.com>
2020-04-15 16:54:45 +0530

    Task 5 of Python class 5

---
+++ Shubhang Vats/Python-Task 5/question3.py
@@ -0,0 +1,21 @@
+# Question - 3
+
+'''
+
+3. Make digital Clock and run it for 5 sec.
+    Output:
+            16:39:08
+                :09
+                :10
+                :11
+                :12
+'''
+import time
+
+
+i=0
+while i < 5:
+    t = time.ctime()
+    print(t[11:19])
+    time.sleep(1)
+    i = (i+1)
---
+++ Shubhang Vats/Python-Task 5/question4.py
@@ -0,0 +1,14 @@
+# Question - 4
+
+
+"""
+4. Add anything in tuple.. example: (1,2,3,4) -> new tuple (1,2,3,4,5)
+"""
+
+t1 = (1,2,3,4)
+
+t2= (5,) #if a single element is in tuple so a , is required after it
+
+t3 = t1 + t2
+
+print(t3)
